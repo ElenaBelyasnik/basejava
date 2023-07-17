c89f52cd497783a1cf69c5200ae0524ec00ce958
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-07-17 22:19:20 +0300

    Modify ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,59 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (Objects.equals(storage[i].uuid, uuid)){
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = size();
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size; j++) {
+                    if (j + 1 < size && storage[j+1] != null){
+                        storage[j] = storage[j+1];
+                    } else {
+                        storage[j] = null;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
+        int size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
