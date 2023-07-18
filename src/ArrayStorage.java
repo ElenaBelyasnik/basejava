d148212887aa3880caf2d36df0e8cfac4973aec7
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-07-18 17:36:21 +0300

    Modify ArrayStorage.java: method "save".

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,13 +14,8 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
