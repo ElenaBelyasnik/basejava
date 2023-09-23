637598c08c5910ec219c65c3b5e3ded5a3082462
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-09-23 22:15:41 +0300

    HW6. fix errors

--- test/ru/javawebinar/basejava/storage/MapStorageTest.java
+++ test/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -1,10 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MapStorageTest extends AbstractStorageTest{
 
     public MapStorageTest() {
-        super(new MapStorage());
+        super(new MapUuidStorage());
     }
 }
