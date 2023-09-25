21e2c0fde2dbb571bb93ab590c32a672aa105806
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-09-25 21:14:50 +0300

    HW6 Create comparator.

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 /**
  * Initial resume class
  */
-public class Resume implements Comparable <Resume> {
+public class Resume implements Comparable<Resume> {
 
     // Unique identifier
     private final String uuid;
@@ -22,6 +22,10 @@ public class Resume implements Comparable <Resume> {
 
     public String getUuid() {
         return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     @Override
@@ -47,7 +51,7 @@ public class Resume implements Comparable <Resume> {
 
     @Override
     public int compareTo(Resume o) {
-        int  cmp = fullName.compareTo(o.fullName);
+        int cmp = fullName.compareTo(o.fullName);
         return cmp != 0 ? cmp : uuid.compareTo(o.uuid);
     }
 }
