ed2503717ce0873fea2e52c4c693e43061593af9
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-11-08 21:22:13 +0300

    Add class MainFile.java

---
+++ src/MainFile.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) throws IOException {
+        String filePath = ".\\.gitignore";
+
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+        File dir = new File("./src/ru/javawebinar/basejava");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        assert list != null;
+        for (String name : list) {
+            System.out.println(name);
+        }
+
+
+        try (FileInputStream fis = new FileInputStream(filePath)){
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } 
+    }
+}
