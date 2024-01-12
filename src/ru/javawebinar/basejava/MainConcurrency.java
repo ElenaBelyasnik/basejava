edbe03dcd145ac300f83d7f44d1499d876c484fb
Elena Belyasnik <elena.belyasnik@yandex.ru>
2024-01-13 01:48:34 +0300

    Lesson 11

---
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -0,0 +1,58 @@
+package ru.javawebinar.basejava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainConcurrency {
+    public static final int THREADS_NUMBER = 10000;
+    private static int counter;
+    private static final Object LOCK = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread thread0 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(getName() + ", " + getState());
+                throw new IllegalStateException();
+            }
+        };
+        thread0.start();
+
+        new Thread(() -> System.out.println(Thread.currentThread().getName()
+                + ", " + Thread.currentThread().getState())).start();
+
+        System.out.println(thread0.getState());
+
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            Thread thread = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(counter);
+    }
+
+//    private synchronized void inc() {
+    private void inc() {
+ //       synchronized (MainConcurrency.class) {
+ //       double a = Math.sin(13.);
+ //       synchronized (this) {
+            counter++;
+
+//        }
+    }
+}
