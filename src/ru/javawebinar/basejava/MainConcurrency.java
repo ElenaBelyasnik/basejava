ec4a7f077c7b0c5fa21ff0dec115775805fc2b99
Elena Belyasnik <elena.belyasnik@yandex.ru>
2024-02-01 22:39:49 +0300

    Lesson 12 Concurrency

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,12 +1,31 @@
 package ru.javawebinar.basejava;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private static int counter;
-    private static final Object LOCK = new Object();
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+    //private static final Object LOCK = new Object();
+    private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock reentrantReadWriteLock =
+            new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -14,7 +33,7 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
+                //throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -26,17 +45,31 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        //ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        CompletionService completionService = new ExecutorCompletionService(executorService);
+
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+            SimpleDateFormat sdf = new SimpleDateFormat();
+            Future<Integer> future = executorService.submit(() ->
+
+//            Thread thread = new Thread(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
                 }
+                latch.countDown(); // синхронизатор
+                return counter;
             });
-            thread.start();
-            threads.add(thread);
+//            thread.start();
+//            threads.add(thread);
         }
 
+/*
         threads.forEach(t -> {
             try {
                 t.join();
@@ -44,19 +77,29 @@ public class MainConcurrency {
                 e.printStackTrace();
             }
         });
-        System.out.println(counter);
+*/
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+        //System.out.println(counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
         //LazySeingleton.getInstance();
 
     }
 
 
-    private synchronized void inc() {
- //   private void inc() {
- //       synchronized (MainConcurrency.class) {
- //       double a = Math.sin(13.);
- //       synchronized (this) {
-            counter++;
-
+    //    private synchronized void inc() {
+    private void inc() {
+        //   private void inc() {
+        //       synchronized (MainConcurrency.class) {
+        //       double a = Math.sin(13.);
+        //       synchronized (this) {
+//        lock.lock();
+//        try {
+        atomicCounter.incrementAndGet();
+//            counter++;
+//        } finally {
+//            lock.unlock();
+//        }
 //        }
     }
 }
