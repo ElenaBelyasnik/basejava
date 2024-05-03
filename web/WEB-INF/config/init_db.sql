dcb8caffbd0864f165b0d1abbb6fbce71e247a20
Elena Belyasnik <elena.belyasnik@yandex.ru>
2024-05-03 20:44:56 +0300

    Lesson 16 HW16, Build and deploy.

---
+++ web/WEB-INF/config/init_db.sql
@@ -0,0 +1,29 @@
+DROP TABLE section;
+DROP TABLE contact;
+DROP TABLE resume;
+
+CREATE TABLE resume
+(
+    uuid      CHAR(36) PRIMARY KEY NOT NULL,
+    full_name TEXT                 NOT NULL
+);
+
+CREATE TABLE contact
+(
+    id          SERIAL,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    value       TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX contact_uuid_type_index
+    ON contact (resume_uuid, type);
+
+CREATE TABLE section
+(
+    id          SERIAL PRIMARY KEY,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    content     TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX section_idx
+    ON section (resume_uuid, type);
